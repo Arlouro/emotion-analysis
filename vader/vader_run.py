ee9bc4fd82909f53000a0a88eea0f9f9a2ccafe9
André Louro <andrelouro02@gmail.com>
2026-04-17 00:09:45 +0100

    csv reading and saving

--- vader/vader_run.py
+++ vader/vader_run.py
@@ -1,28 +1,37 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import csv
 
 analyzer = SentimentIntensityAnalyzer()
 
-texts = [
-    "I love this product! It's amazing.",
-    "This is the worst experience I've ever had.",
-    "It's okay, not great but not terrible either.",
-    "I am not very happy, but I am also not especially sad"
-]
+texts = []
+
+with open("./vader/llm_comparative_analysis.csv", "r", encoding="utf-8") as f:
+    reader = csv.reader(f)
+    next(reader)  # skip header
+    for row in reader:
+        texts.append(row[5])
 
 # CSV header
 with open("./vader/results/sentiment_analysis.csv", "w", encoding="utf-8") as f:
     f.write("Text,Negative,Neutral,Positive,Compound")
+    f.write("\n")
+
+results = []
 
 for text in texts:
     sentiment = analyzer.polarity_scores(text)
     print(f"Text: {text}")
     print(f"Sentiment: {sentiment}")
+    
+    # save to array
+    results.append([text, sentiment['neg'], sentiment['neu'], sentiment['pos'], sentiment['compound']])
 
     # Save to txt file
     """ with open("./vader/results/sentiment_analysis.txt", "a", encoding="utf-8") as f:
         f.write(f"Text: {text}\n")
         f.write(f"Sentiment: {sentiment}\n\n") """
     
-    # Save to CSV file
-    with open("./vader/results/sentiment_analysis.csv", "a", encoding="utf-8") as f:
-        f.write(f"\n{text},{sentiment['neg']},{sentiment['neu']},{sentiment['pos']},{sentiment['compound']}")
+# Save to CSV file
+with open("./vader/results/sentiment_analysis.csv", "a", newline="", encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerows(results)
